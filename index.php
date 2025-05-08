2a763645594560f2d2b99a022dd48b890531efd5
mester559 <abdullahaldini77@gmail.com>
2025-05-08 06:40:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello from Render with PHP!";
+$input = file_get_contents('php://input');
+file_put_contents("log.txt", $input . PHP_EOL, FILE_APPEND);
+http_response_code(200);
+echo "Webhook received!";
 ?>
